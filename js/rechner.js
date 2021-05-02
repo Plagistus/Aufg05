edb246f2e524baf29f7d31eebe4ba514899555e7
Johanna Schupp <johanna.schupp@hs-furtwangen.de>
2021-05-02 15:43:00 +0200

    initial commit

---
+++ js/rechner.js
@@ -0,0 +1,69 @@
+'use strict';
+
+function Model() {
+
+    var self = this,
+        rateUSDEUR,
+        rateUSDCHF;
+    let chf;
+
+    self.observable = new Observable();
+
+
+    function getCurrencyFactor() {
+        var xhttp = new XMLHttpRequest();
+        var url = "http://api.currencylayer.com/live?access_key=936ee4281f2f36b5f7658bb477a66675&format=1";
+        xhttp.open("GET", url, true);
+        xhttp.addEventListener("load", transferComplete, false);
+        xhttp.addEventListener("error", transferFailed, false);
+        xhttp.send(null);
+
+        /*
+ //HTTP request aufsetzen
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "text.txt", true);
+        xhttp.addEventListener("load", transferComplete, false);
+        xhttp.addEventListener("error", transferFailed, false);
+ */
+    }
+
+
+    function transferComplete() {
+        rateUSDEUR = JSON.parse(this.responseText).quotes.USDEUR;
+        rateUSDCHF = JSON.parse(this.responseText).quotes.USDCHF;
+        getEUR();
+
+        /*xhttp.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                this.responseText;
+                rate = xhttp.responseText;
+                console.log("Rate: "+rate);
+                getEUR();
+            }
+        };
+        xhttp.send();*/
+    }
+
+    function transferFailed(data) {
+        console.log("Error: " + data);
+    }
+
+
+    //-----------------------------------//
+
+    function getEUR() {
+        var CHFUSD = 1/rateUSDCHF;
+        var dollar = chf * CHFUSD;
+
+        var euro = dollar * rateUSDEUR;
+        euro = euro.toFixed(2);
+        self.observable.notifyObservers(euro);
+    }
+
+    self.setCHF = function(amount) {
+        chf = amount;
+        //getEUR();
+        getCurrencyFactor();
+    };
+
+}
---
+++ js/util.js
@@ -0,0 +1,21 @@
+function Observable() {
+    var observers = [];
+
+    this.addObserver = function(observer) {
+        observers.push(observer);
+    };
+
+    this.removeObserver = function(observer) {
+        observers = observers.filter(function(item) {
+            return item !== observer;
+        });
+    };
+
+    this.notifyObservers = function(total, totalPerPerson, getTip, tipPerPerson) {
+        var i;
+        console.log(observers);
+        for ( i = 0; i < observers.length; i = i + 1) {
+            observers[i].update(total, totalPerPerson, getTip, tipPerPerson);
+        }
+    };
+}
